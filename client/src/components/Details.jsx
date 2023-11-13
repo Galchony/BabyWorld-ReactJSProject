c8e616734e604fa8d262a79a196e2238f8c3ba5d
Galia Angelova <po6tata_na_galia@abv.bg>
2023-11-13 17:38:53 +0200

    Add details

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -1,17 +1,33 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-export default function Details({
-   
-}) {
-    const {postId} = useParams();
+import * as postService from "../services/postService";
+
+import styles from "./Details.module.css";
+
+
+export default function Details() {
+  const { postId } = useParams();
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    postService.getOne(postId).then((result) => setPost(result));
+  }, [postId]);
+
   return (
     <section>
       <h1>Post Details</h1>
       <div>
-        <img src="" />
+        <img src={post.imageUrl} />
+        <h3 className={styles.title}> {post.title}</h3>
+        <p>Category: {post.category}</p>
       </div>
 
-      <button type="button">Delete</button>
+      <p>{post.description}</p>
+
+      <div className={styles.container}>
+      <button type="button" className={styles.buttons}>Delete</button>
+      </div>
     </section>
   );
 }
